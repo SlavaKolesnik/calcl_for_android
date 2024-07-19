37405f08ee40f41676b60b16267a2b53806013f4
slava1995 <slava1995kolesnik@gmail.com>
2024-07-19 21:36:04 +0300

    create findiewyd

--- app/src/main/java/android/example/calculation/MainActivity.kt
+++ app/src/main/java/android/example/calculation/MainActivity.kt
@@ -1,6 +1,8 @@
 package android.example.calculation
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val zeroButton = findViewById<Button>(R.id.zero_button)
+        val oneButton = findViewById<Button>(R.id.one_button)
+        val twoButton = findViewById<Button>(R.id.two_button)
+        val threeButton = findViewById<Button>(R.id.three_button)
+        val fourButton = findViewById<Button>(R.id.four_button)
+        val fiveButton = findViewById<Button>(R.id.five_button)
+        val sixButton = findViewById<Button>(R.id.six_button)
+        val sevenButton = findViewById<Button>(R.id.seven_button)
+        val eightButton = findViewById<Button>(R.id.eight_button)
+        val nineButton = findViewById<Button>(R.id.nine_button)
+
+        val equalButton = findViewById<Button>(R.id.equal_button)
+        val plusButton = findViewById<Button>(R.id.plus_button)
+        val minusButton = findViewById<Button>(R.id.minus_button)
+        val multiplyButton = findViewById<Button>(R.id.multiply_button)
+
+        val resultTextView = findViewById<TextView>(R.id.result_textView)
+
+        resultTextView.text = "Blabla"
     }
 }
